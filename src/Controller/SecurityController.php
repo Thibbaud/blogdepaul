41086ee2b504f6dd7aff0035212a5fc7af31de73
Thibaud Levy <titi@desktop-uc3pb6h.home>
2019-06-27 12:49:22 +0200

    mise en place des commentaires

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\AccountType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -30,4 +33,35 @@ class SecurityController extends AbstractController
         // controller can be blank: it will never be executed!
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
+
+    /**
+     * Permet d'afficher et traiter le form de modif du profil
+     *
+     *
+     * @Route("/account/profile", name="account_profile")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function profile(Request $request, ObjectManager $manager) {
+        $user = $this->getUser();
+        $form = $this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash('success',
+                "Les données ont bien été modifiées !");
+
+            return $this->redirectToRoute('blog');
+        }
+
+        return $this->render('security/profile.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 }
+
